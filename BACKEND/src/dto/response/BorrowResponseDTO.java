320b40c31ce7a7d63c6e539641da178a56c0ecc6
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 22:30:31 +0700

    Update BorrowResponseDTO.java

--- BACKEND/src/dto/response/BorrowResponseDTO.java
+++ BACKEND/src/dto/response/BorrowResponseDTO.java
@@ -1,28 +1,38 @@
-package dto.response;
+package com.library.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BorrowResponseDTO {
+
     private String transactionId;
     private String readerId;
+    private String readerName;
     private String staffId;
+    private String staffName;
     private LocalDate dateBorrow;
     private LocalDate dueDate;
+    private List<BorrowDetailDTO> borrowDetails;
 
-    public BorrowResponseDTO() {}
-
-    public String getTransactionId() { return transactionId; }
-    public void setTransactionId(String transactionId) { this.transactionId = transactionId; }
-
-    public String getReaderId() { return readerId; }
-    public void setReaderId(String readerId) { this.readerId = readerId; }
-
-    public String getStaffId() { return staffId; }
-    public void setStaffId(String staffId) { this.staffId = staffId; }
-
-    public LocalDate getDateBorrow() { return dateBorrow; }
-    public void setDateBorrow(LocalDate dateBorrow) { this.dateBorrow = dateBorrow; }
-
-    public LocalDate getDueDate() { return dueDate; }
-    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BorrowDetailDTO {
+        private String borrowDetailId;
+        private String bookId;
+        private String bookTitle;
+        private String bookStatus;
+        private LocalDate dateIssued;
+        private LocalDate actualReturnDate;
+    }
 }
