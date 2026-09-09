e976f9041b03eb1442c1079908a29a987fa0458c
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 21:12:32 +0700

    Create StaffService.java

---
+++ BACKEND/src/service/StaffService.java
@@ -0,0 +1,14 @@
+package service;
+
+public class StaffService {
+
+    public boolean createStaff(String staffName, String email, String adminId) {
+        // TODO: Add new staff record into Staff table
+        return true;
+    }
+
+    public boolean updateStaffStatus(String staffId, String status) {
+        // TODO: Update staff active status
+        return true;
+    }
+}
