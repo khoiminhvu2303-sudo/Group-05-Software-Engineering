f4a2fc038395d4a5bee7009c2aad6413e64164d4
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 20:36:51 +0700

    Create ReturnService.java

---
+++ BACKEND/Src/main/java/com/library/service/ReturnService.java
@@ -0,0 +1,11 @@
+package service;
+
+import dto.request.ProcessReturnDTO;
+
+public class ReturnService {
+
+    public boolean processReturnBook(ProcessReturnDTO request) {
+        // TODO: Update actualReturnDate and bookStatus in BorrowDetail table
+        return true;
+    }
+}
