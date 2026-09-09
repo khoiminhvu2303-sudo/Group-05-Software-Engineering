53591ffb6c08ee538ad48c73079888c1c669c6a4
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 21:11:43 +0700

    Create NotificationService.java

---
+++ BACKEND/src/service/NotificationService.java
@@ -0,0 +1,12 @@
+package service;
+
+public class NotificationService {
+
+    public void sendBorrowReminderEmail(String readerId) {
+        // TODO: Send return deadline reminder email to reader
+    }
+
+    public void sendOverdueSMS(String readerId) {
+        // TODO: Send overdue notification via SMS
+    }
+}
