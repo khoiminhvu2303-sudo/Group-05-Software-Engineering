25d04bebb16a97b8ba9f18d533f9c4601d572e7c
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 21:08:02 +0700

    Update ReaderService.java

--- BACKEND/src/service/ReaderService.java
+++ BACKEND/src/service/ReaderService.java
@@ -1,1 +1,22 @@
+package service;
 
+import dto.request.RegisterReaderDTO;
+import dto.response.ReaderResponseDTO;
+
+public class ReaderService {
+
+    public ReaderResponseDTO registerReader(RegisterReaderDTO request) {
+        // TODO: Save new reader entity to Reader table
+        ReaderResponseDTO response = new ReaderResponseDTO();
+        response.setReaderId("RD00000001");
+        response.setFullName(request.getFullName());
+        response.setEmail(request.getEmail());
+        response.setStatus("Active");
+        return response;
+    }
+
+    public ReaderResponseDTO getReaderById(String readerId) {
+        // TODO: Find reader record by readerId
+        return new ReaderResponseDTO();
+    }
+}
