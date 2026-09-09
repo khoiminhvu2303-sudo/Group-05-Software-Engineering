e958101f7c7eb541db07472718f3e1f6873cc125
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 22:25:58 +0700

    Update LoginDTO.java

--- BACKEND/src/dto/request/LoginDTO.java
+++ BACKEND/src/dto/request/LoginDTO.java
@@ -1,19 +1,20 @@
-package dto.request;
+package com.library.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
+
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
     private String password;
-
-    public LoginDTO() {}
-
-    public LoginDTO(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
