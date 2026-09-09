e98e3d45682c35b7deb2f2ee8bc36fe0f053bf15
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-09 20:38:09 +0700

    Create ReportService.java

---
+++ BACKEND/Src/main/java/com/library/service/ReportService.java
@@ -0,0 +1,16 @@
+package service;
+
+import java.time.LocalDate;
+
+public class ReportService {
+
+    public byte[] exportBorrowStatistics(LocalDate startDate, LocalDate endDate) {
+        // TODO: Export borrowing statistics report
+        return new byte[0];
+    }
+
+    public byte[] exportFineRevenue(LocalDate startDate, LocalDate endDate) {
+        // TODO: Export fine revenue report
+        return new byte[0];
+    }
+}
