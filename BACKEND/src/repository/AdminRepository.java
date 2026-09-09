b201284edc57ea686aafb1c66295de12963a68f0
baonlg798158-max <baonlg798158@ut.edu.vn>
2026-09-09 21:15:04 +0700

    Create AdminRepository.java

---
+++ BACKEND/src/repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package com.library.repository;
+
+import com.library.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, String> {
+    Optional<Admin> findByNameLoginAdmin(String nameLoginAdmin);
+    Optional<Admin> findByEmail(String email);
+    boolean existsByNameLoginAdmin(String nameLoginAdmin);
+}
