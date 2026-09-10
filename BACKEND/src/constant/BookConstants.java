33eb46f2da3ababae340e1d4ee09d2da1609af56
baonlg798158-max <baonlg798158@ut.edu.vn>
2026-09-10 15:26:15 +0700

    Create BookConstants.java

---
+++ BACKEND/src/constant/BookConstants.java
@@ -0,0 +1,17 @@
+package com.library.constant;
+
+public final class BookConstants {
+
+    private BookConstants() {}
+
+    public static final String BOOK_ID_PREFIX  = "BK";
+    public static final String COPY_ID_PREFIX  = "CP";
+    public static final String BARCODE_PREFIX  = "BC";
+
+    public static final int SEARCH_MIN_KEYWORD_LENGTH = 2;
+    public static final int DEFAULT_STOCK_QUANTITY    = 1;
+    public static final int MIN_PUBLISH_YEAR          = 1800;
+
+    public static final String DEFAULT_LANGUAGE       = "Tiếng Việt";
+    public static final String COVER_IMAGE_DEFAULT    = "default_book_cover.jpg";
+}
