5f97d2f510e9f5767376a5d6ab0ee6f806f93565
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-11 20:54:14 +0700

    Create AuthResponseDTO.java

---
+++ BACKEND/src/dto/response/AuthResponseDTO.java
@@ -0,0 +1,18 @@
+package com.library.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDTO {
+
+    private String token;
+    private String type; // Bearer
+    private String username;
+    private String role;
+}
