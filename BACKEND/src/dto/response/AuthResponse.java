dc929c1a350b2a385a5fc44111ddbc60aabc74cc
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-11 22:45:19 +0700

    Update and rename AuthResponseDTO.java to AuthResponse.java

--- BACKEND/src/dto/response/AuthResponse.java
+++ BACKEND/src/dto/response/AuthResponse.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthResponseDTO {
+public class AuthResponse {
 
-    private String token;
-    private String type; // Bearer
-    private String username;
-    private String role;
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiresIn;
+    private Object user;
 }
