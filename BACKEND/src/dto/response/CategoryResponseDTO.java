be3d9637ca96e048c4ec85f316d84955ce48ff68
khoiminhvu2303-sudo <khoiminhvu2303@gmail.com>
2026-09-11 20:44:39 +0700

    Create CategoryResponseDTO.java

---
+++ BACKEND/src/dto/response/CategoryResponseDTO.java
@@ -0,0 +1,17 @@
+package com.library.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponseDTO {
+
+    private String categoryId;
+    private String categoryName;
+    private String describe;
+}
