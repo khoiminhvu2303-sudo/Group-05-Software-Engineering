cd5f45bbfef3e1b739a60e5d9b2834d0612db875
Hoàng đỗ gia huy <huyhdg3013@ut.edu.vn>
2026-09-12 20:42:54 +0700

    Update cardStatus.sql

--- DATABASE/Trigger/cardStatus.sql
+++ DATABASE/Trigger/cardStatus.sql
@@ -1,28 +1,30 @@
 DELIMITER //
 
+DROP TRIGGER IF EXISTS after_insert_finereceipt //
 CREATE TRIGGER after_insert_finereceipt
-AFTER INSERT ON FineReceipt
+AFTER INSERT ON DetailFineReceipt
 FOR EACH ROW
 BEGIN
     DECLARE reader_id INT;
+
     -- Lấy readerID từ BorrowDetail -> BorrowRecord
     SELECT br.readerID INTO reader_id
     FROM BorrowDetail bd
     JOIN BorrowRecord br ON bd.borrowID = br.borrowID
     WHERE bd.borrowDetailID = NEW.borrowDetailID;
 
-    -- Nếu có bất kỳ fine nào chưa trả của reader này, khóa tài khoản
+    -- Nếu có bất kỳ fine nào chưa trả (Status = Pending) của reader này, khóa tài khoản
     IF EXISTS (
         SELECT 1
-        FROM FineReceipt fr
+        FROM DetailFineReceipt fr
         JOIN BorrowDetail bd ON fr.borrowDetailID = bd.borrowDetailID
         JOIN BorrowRecord br ON bd.borrowID = br.borrowID
         WHERE br.readerID = reader_id
-          AND fr.status = 'Pending'
+          AND fr.Status = 'Pending'
     ) THEN
         UPDATE Reader
-        SET cardStatus = 'Suspended'
-        WHERE readerID = reader_id;
+        SET Status = 'Suspended'
+        WHERE ReaderID = reader_id;
     END IF;
 END; //
 
