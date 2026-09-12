543dcf75f46763a50843f39cd60b8408288801dc
baonlg798158-max <baonlg798158@ut.edu.vn>
2026-09-12 20:44:13 +0700

    Update manageFine.sql

--- DATABASE/Query/manageFine.sql
+++ DATABASE/Query/manageFine.sql
@@ -1,19 +1,21 @@
-SELECT 
-    fr.fineID,
-    b.title,
-    bc.barcode,
-    fr.reason,
-    fr.amount,
-    fr.issuedDate
-FROM FineReceipt fr
-JOIN BorrowDetail bd ON fr.borrowDetailID = bd.borrowDetailID
-JOIN BorrowRecord br ON bd.borrowID = br.borrowID
-JOIN BookCopy bc ON bd.copyID = bc.copyID
-JOIN Book b ON bc.bookID = b.bookID
-WHERE br.readerID = ? AND fr.status = 'Pending';
+USE LIBRARY_MANAGEMENT_SYSTEM;
 
-UPDATE FineReceipt 
-SET status = 'Paid',
-    paidDate = NOW(),
-    staffID = ?
-WHERE fineID = ?;
+SELECT 
+    dfr.DetailFineID AS FineID,
+    b.Title,
+    bc.Barcode,
+    dfr.Reason,
+    dfr.Amount,
+    dfr.IssuedDate
+FROM DetailFineReceipt dfr
+JOIN BorrowDetail bd ON dfr.BorrowDetailID = bd.BorrowDetailID
+JOIN BorrowRecord br ON bd.BorrowID = br.BorrowID
+JOIN BookCopy bc ON bd.CopyID = bc.CopyID
+JOIN Book b ON bc.BookID = b.BookID
+WHERE br.ReaderID = ? AND dfr.Status = 'Pending';
+
+UPDATE DetailFineReceipt 
+SET Status = 'Paid',
+    PaidDate = NOW(),
+    StaffID = ?
+WHERE DetailFineID = ?;
